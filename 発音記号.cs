8ececbe4740c4ec5bfdf2eb1a71f7ed1b8ca7893
yuuki1293 <yuuki1293@outlook.jp>
2020-12-18 20:40:41 +0900

    文字の数を取得できるようにした

--- 発音記号.cs
+++ 発音記号.cs
@@ -1,67 +1,53 @@
 ﻿using System;
 using System.IO;
 using ClosedXML.Excel;
-
-class 発音記号
+namespace 発音記号
 {
-    static void Main(string[] args)
+    class 発音記号
     {
-        string exeファイルのパス = AppDomain.CurrentDomain.BaseDirectory.TrimEnd('\\');
-        DirectoryInfo 作業ディレクトリ = new DirectoryInfo(exeファイルのパス);
-        さかのぼる(ref 作業ディレクトリ, 3);
-        Excel操作 ex = new Excel操作(作業ディレクトリ);
-        ex.書き込み("A1", "Hello World");
-        ex.シートを削除("Sheet2");
-        ex.セーブ();
-    }
-
-    static void さかのぼる(ref DirectoryInfo path, int 回数)
-    {
-        for (int i = 0; i < 回数; i++)
+        static void Main(string[] args)
         {
-            path = path.Parent;
+            string exeファイルのパス = AppDomain.CurrentDomain.BaseDirectory.TrimEnd('\\');
+            DirectoryInfo 作業ディレクトリ = new DirectoryInfo(exeファイルのパス);
+            さかのぼる(ref 作業ディレクトリ, 3);
+            Excel操作 ex = new Excel操作(作業ディレクトリ);
+            int length = ex.読み取り();
+            Console.WriteLine(length);
+        }
+
+        static void さかのぼる(ref DirectoryInfo path, int 回数)
+        {
+            for (int i = 0; i < 回数; i++)
+            {
+                path = path.Parent;
+            }
         }
     }
-}
 
-class Excel操作
-{
-    private string excelのパス;
-    private XLWorkbook workbook;
-    private IXLWorksheet worksheet;
-    public Excel操作(DirectoryInfo 作業ディレクトリ)
+    class Excel操作
     {
-        excelのパス = 作業ディレクトリ.ToString() + @"\発音記号.xlsx";
-        workbook = new XLWorkbook(excelのパス);
-        worksheet = workbook.Worksheet("Sheet1");
-    }
-    public void 書き込み(string セル, string 値)
-    {
-        worksheet.Cell(セル).Value = "Hello";
-    }
-    public void 書き込み(string セル, int 値)
-    {
-        worksheet.Cell(セル).SetValue(値);
+        private string excelのパス;
+        private XLWorkbook workbook;
+        private IXLWorksheet worksheet;
+        int lastRow;
+        public Excel操作(DirectoryInfo 作業ディレクトリ)
+        {
+            excelのパス = 作業ディレクトリ.ToString() + @"\発音記号.xlsx";
+            workbook = new XLWorkbook(excelのパス);
+            worksheet = workbook.Worksheet("Sheet1");
+        }
+        public int 読み取り()
+        {
+            lastRow = worksheet.LastRowUsed().RowNumber();
+            return lastRow;
+        }
     }
 
-    public void セーブ()
+    class Web操作
     {
-        workbook.Save();
-    }
-    public void セーブ(string 名前)
-    {
-        workbook.SaveAs(名前);
-    }
-    public void シートを削除(string 名前)
-    {
-        workbook.Worksheet(名前).Delete();
-    }
-}
+        public Web操作()
+        {
 
-class Web操作
-{
-    public Web操作()
-    {
-
+        }
     }
 }
